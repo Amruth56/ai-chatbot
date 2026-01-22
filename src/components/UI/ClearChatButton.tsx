3f454ab9bd73ec7b0e87999a41a1b317c05d0fda
Amruth <ammumandappa@gmail.com>
2026-01-22 16:09:51 +0530

    added clear chst button

--- src/components/UI/ClearChatButton.tsx
+++ src/components/UI/ClearChatButton.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useChat } from '../../context/ChatContext'
 
 const ClearChatButton = () => {
+  const {clearChat} = useChat();
+
   return (
-    <div>ClearChatButton</div>
+    <div>
+      <button className='text-sm px-3 py-1 rounded border hover:bg-gray-100 dark:hover:bg-gray-700'
+      onClick={clearChat}
+      >
+        Clear Chat
+      </button>
+    </div>
   )
 }
 
